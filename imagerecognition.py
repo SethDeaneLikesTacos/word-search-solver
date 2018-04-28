9d513fe21192947324525d84835a853b4662f2c1
SethDeaneLikesTacos <30837275+SethDeaneLikesTacos@users.noreply.github.com>
2018-04-28 19:04:40 -0400

    Updated Search Algorithm need to add updated pics

--- imagerecognition.py
+++ imagerecognition.py
@@ -1,6 +1,10 @@
 import numpy as np
 from PIL import Image as PILImage
 
+grid_start_row = 58
+grid_start_col = 18
+grid_end_row = 455
+grid_end_col = 547
 
 def determineletter(puzzle_file):
     """
@@ -8,10 +12,7 @@ def determineletter(puzzle_file):
     :param letter_file: The filepath of a picture to check the letter of.
     :return: char of the letter that was found
     """
-    grid_start_row = 58
-    grid_start_col = 18
-    grid_end_row = 455
-    grid_end_col = 547
+
 
     # for the time being, pick one letter to read into a list
     letter_png_list = ["Resources/testletters/a.png",
@@ -44,53 +45,54 @@ def determineletter(puzzle_file):
     # read puzzle into a list
     i = PILImage.open(puzzle_file)
     puzzle = np.array(i)
+    puzzle = threshold(puzzle)
 
     # loop through every letter in the alphabet
-    for i in range(4,5):
+    for i in range(12,13):
     #for i in range(26):
-        print(i)
+        print("Searching for: " + letter_png_list[i][-5])
+
+        # Read letter into a list
         letimg = PILImage.open(letter_png_list[i])
         letter = np.array(letimg)
+        letter = threshold(letter)
+
+        # Determine value for the dimensions of the letter to look for
         letter_rows = len(letter)            # number of rows in letter png
         letter_cols = len(letter[0])         # number of cols in letter png
         num_pix = letter_rows * letter_cols
 
+        # starting pixels
+        for row_puz in range(grid_start_row, grid_end_row):
+            for col_puz in range(grid_start_col, grid_end_col):
 
-        letter = threshold(letter, letter_rows, letter_cols)
-
-
-        # loop through every pixel in the puzzle
-        for row_puz in range(grid_start_row, grid_end_row, 30):
-            for col_puz in range(grid_start_col, grid_end_col, 32):
-
+                # grab a cross section of the puzzle to compare to our letter
                 puzbox = puzzle[row_puz : row_puz+letter_rows , col_puz : col_puz+letter_cols]
-                puzbox = threshold(puzbox, letter_rows, letter_cols)
 
                 count = 0
                 for r in range(letter_rows):
                     for c in range(letter_cols):
                         if puzbox[r][c] == letter[r][c]:
                             count += 1
-                print(count / num_pix)
 
-                #print(puzbox)
 
-                #if ((row_puz * col_puz) % 10132 == 0):
-                #    print(np.mean(letter == puzbox))
-
-                #if np.mean(letter == puzbox) > .71:
-                #    print("Found: " + letter_png_list[i][-5])
-                    
-
-                if count / num_pix > .9:
-                    print("Found: " + letter_png_list[i][-5] + " @ " + str(col_puz) + " " + str(row_puz))
-                #if np.array_equal(letter, puzbox):
-                #    print("Found: " + letter_png_list[i][-5] + " @ " + str(col_puz) + " " + str(row_puz))
-
+                # If the percentage of the matched pixels is above a threshold, we can say the letter was found
+                if count / num_pix > .95:
+                    grid_col, grid_row = togrid(row_puz, col_puz)
+                    print("Found: " + letter_png_list[i][-5] + \
+                          " @ " + str(grid_row) + " " + str(grid_col) + \
+                          " with " + str(count / num_pix) + " precision")
     return
 
 
-def threshold(array, rr, cc):
+def threshold(array):
+    """
+
+    :param array: the array
+    :return:
+    """
+    rr = len(array)  # number of rows in letter png
+    cc = len(array[0])  # number of cols in letter png
 
     new_array = np.empty([rr, cc])
 
@@ -108,4 +110,9 @@ def threshold(array, rr, cc):
             #print(str(c) + " " + str(r) + " " + str(new_array[r][c]) + " " + str(array[r][c][0]))
 
     return new_array
+
+def togrid(row, col):
+    grid_col = (col - grid_start_col) % 32
+    grid_row = (row - grid_start_row) % 34
+    return grid_col, grid_row
 
