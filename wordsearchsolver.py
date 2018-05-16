98386111d456870503c0cc524dba7c4b41e5448a
SethDeaneLikesTacos <30837275+SethDeaneLikesTacos@users.noreply.github.com>
2018-05-16 11:02:54 -0400

    Updated to search from pdf
    Will now search from a pdf and will output a png with the words highlighted.

--- wordsearchsolver.py
+++ wordsearchsolver.py
@@ -2,32 +2,36 @@ import numpy as np
 from PIL import Image, ImageDraw
 import pytesseract
 import re
+import pdf2image
 
 # path to tesseract (necessary for tesseract)
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
 
+# color type used by pytesseract
+RGB_VAL = "RGB"
+
 # dimensions of the puzzle to be solved
 width, height = 14, 14
 
-RGB_VAL = "RGB"
-
 # boundaries of the letter grid
-grid_start_row = 89
-grid_start_col = 30
-grid_end_row = 666
-grid_end_col = 610
+grid_start_row = 244
+grid_start_col = 95
+grid_end_row = 1424
+grid_end_col = 1211
 
 
-def image_to_array(path):
-
+def image_to_array(puz_image):
+    """
+    creates a numpy matrix of the puzzle
+    :param puz_image: the image object of entire puzzle
+    :return: numpy matrix of the word search
+    """
     # read image into array
-    img = Image.open(path)
-    puzzle = np.array(img)
+    puzzle = np.array(puz_image)
 
     # grab a cross section of the puzzle to identify and convert to image
     puzbox = puzzle[grid_start_row : grid_end_row , grid_start_col : grid_end_col]
     puzbox_img = Image.fromarray(puzbox, RGB_VAL)
-    puzbox_img.save("temp_puzzle.png")
 
     # identify the letter of the cross section
     text = pytesseract.image_to_string(puzbox_img, config="--psm 6", lang='eng')
@@ -52,28 +56,25 @@ def image_to_array(path):
     for i in range(width):
         for j in range(height):
             matrix[i][j] = text[i][j]
-    print(matrix)
     return matrix
 
 
-def search_words(path):
+def search_words(puz_image):
     """
     creates a list of the words that will be found in the puzzle.
-    :param path: path of the puzzle to be solved
-    :return:
+    :param puz_image: image object of entire puzzle
+    :return: list of words to find in the puzzle
     """
-    # dimensions
-    fw_x1, fw_y1 = 620, 80
-    fw_x2, fw_y2 = 803, 669
+    # dimensions where words to search for are
+    fw_x1, fw_y1 = 1227, 244
+    fw_x2, fw_y2 = 1637, 1424
 
     # read image into array
-    img = Image.open(path)
-    puzzle = np.array(img)
+    puzzle = np.array(puz_image)
 
     # grab a cross section of the puzzle to identify and convert to image
     wordbox = puzzle[ fw_y1: fw_y2, fw_x1: fw_x2]
     wordbox_img = Image.fromarray(wordbox, RGB_VAL)
-    wordbox_img.save("temp_words.png")
 
     # translate picture to text
     text = pytesseract.image_to_string(wordbox_img, config="--psm 1", lang='eng')
@@ -106,15 +107,15 @@ def print_matrix(matrix):
         print()
 
 
-def findword(matrix, words_to_find, path):
+def findword(matrix, words_to_find, puz_image):
     """
-    Loops through each in the word search to see if it matches the first letter of
-    the word being searched for. If first letter in the matrix matches the first
-    letter being searched for, then findneighbors() is called.
-    :return: None
+    locates and highlights the words found in the puzzle
+    :param matrix: numpy matrix of the puzzle letters
+    :param words_to_find: list of the words to be found in the puzzle
+    :param puz_image: image object of entire puzzle
+    :return: image object with highlighted words
     """
-    im = Image.open(path)
-    draw = ImageDraw.Draw(im)
+    draw = ImageDraw.Draw(puz_image)
 
     for word in range(len(words_to_find)):
         word = words_to_find[word]
@@ -196,129 +197,24 @@ def findword(matrix, words_to_find, path):
         if found == 0:
             print("!! WORD NOT FOUND !!")
 
-    im.save("out.png")
-    return
+    return puz_image
 
 
 def draw_line(draw, x1, x2, y1, y2):
-    # list of starting rows and columns for each letter
-    row_nums = [89, 131, 172, 214, 255, 295, 337, 379, 419, 460, 503, 543, 584, 626]
-    col_nums = [30, 71, 113, 155, 196, 237, 279, 321, 361, 403, 445, 487, 528, 570]
-
-    # draw lines highlighting the words found
+    """
+    draw lines highlighting the words that were found
+    :param draw: the image to draw on
+    :param x1: starting x location
+    :param x2: ending x location
+    :param y1: starting y location
+    :param y2: ending y location
+    :return:
+    """
     draw.line(
-        (col_nums[x1] + 20, row_nums[y1] + 20,
-         col_nums[x2] + 20, row_nums[y2] + 20)
+        (grid_start_col + x1 * 80 + 40, grid_start_row + y1 * 86 + 20,
+         grid_start_col + x2 * 80 + 40, grid_start_row + y2 * 86 + 20)
         , fill=(255, 0, 0, 255), width=2)
     return
-
-
-def find_neighbors(w, let, row, matrix):
-    """
-    Search every direction around the matched letter. If the next letter in the matrix
-    matches the next letter in the word to find, then continue to search for the rest
-    of the word in that direction.
-    :param w: Current word being searched for.
-    :param let: Column of the matched letter.
-    :param row: Row of the matched letter.
-    :return: final x, y pair and whether or not the word was found
-    """
-    x, y = let, row
-    n = 1  # character we are checking
-    found = 0
-
-
-    # North
-    if row-n > 0 and matrix[row-n][let] == w[n]:
-        for q in range(len(w)):
-            if row-n > 0 and matrix[row-q][let] == w[q]:
-                # print(str(q) + ": " + matrix[row-q][let] + "|" + w[q])
-                if q == len(w)-1:
-                    x = x
-                    y -= q
-                    found = 1
-                    return x, y, found
-
-    # North East
-    elif row-n > 0 and let+n < width and matrix[row-n][let+n] == w[n]:
-        for q in range(len(w)):
-            if row-q > 0 and let+q < width and matrix[row-q][let+q] == w[q]:
-                # print(str(q) + ": " + matrix[row-q][let+q] + "|" + w[q])
-                if q == len(w)-1:
-                    x += q
-                    y -= q
-                    found = 1
-                    return x, y, found
-
-    # East
-    elif let+n < width and matrix[row][let+n] == w[n]:
-        for q in range(len(w)):
-            if let+q < width and matrix[row][let+q] == w[q]:
-                # print(str(q) + ": " + matrix[row][let+q] + "|" + w[q])
-                if q == len(w)-1:
-                    x += q
-                    y = y
-                    found = 1
-                    return x, y, found
-    # South East
-    elif row+n < height and let+n < width and matrix[row+n][let+n] == w[n]:
-        for q in range(len(w)):
-            if row+q < height and let+q < width and matrix[row+q][let+q] == w[q]:
-                # print(str(q) + ": " + matrix[row+q][let+q] + "|" + w[q])
-                if q == len(w)-1:
-                    x += q
-                    y += q
-                    found = 1
-                    return x, y, found
-
-    # South
-    elif row+n < height and matrix[row+n][let] == w[n]:
-        for q in range(len(w)):
-            if row+q < height and matrix[row+q][let] == w[q]:
-                # print(str(q) + ": " + matrix[row+q][let] + "|" + w[q])
-                if q == len(w)-1:
-                    x = x
-                    y += q
-                    found = 1
-                    return x, y, found
-
-    # South West
-    elif row+n < height and let-n > 0 and matrix[row+n][let-n] == w[n]:
-        for q in range(len(w)):
-            if row+q < height and let-q > 0 and matrix[row+q][let-q] == w[q]:
-                # print(str(q) + ": " + matrix[row+q][let-q] + "|" + w[q])
-                if q == len(w)-1:
-                    x -= q
-                    y += q
-                    found = 1
-                    return x, y, found
-
-    # West
-    elif let-n > 0 and matrix[row][let-n] == w[n]:
-        for q in range(len(w)):
-            if let-q > 0 and matrix[row][let-q] == w[q]:
-                # print(str(q) + ": " + matrix[row][let-q] + "|" + w[q])
-                if q == len(w)-1:
-                    x -= q
-                    y = y
-                    found = 1
-                    return x, y, found
-
-    # North West
-    elif row-n > 0 and let-n > 0 and matrix[row-n][let-n] == w[n]:
-        for q in range(len(w)):
-            if row-q > 0 and let-q > 0 and matrix[row-q][let-q] == w[q]:
-                # print(str(q) + ": " + matrix[row-q][let] + "|" + w[q])
-                if q == len(w)-1:
-                    x -= q
-                    y -= q
-                    found = 1
-                    return x, y, found
-
-    # print("No matching neighbors were found!")
-    return 0, 0, found
-
-
 
 
 def main():
@@ -326,16 +222,14 @@ def main():
     Main function of the word search solver program.
     :return:
     """
-    # path to the puzzle
-    puz_path = "wordsearches\\ws4.png"
-
-
-    array = image_to_array(puz_path)
-    words_to_find = search_words(puz_path)
+    puz_image = pdf2image.convert_from_path('wordsearches\\ws1.pdf')
+    array = image_to_array(puz_image[0])
+    words_to_find = search_words(puz_image[0])
     for i in words_to_find:
         print(i)
     print_matrix(array)
-    findword(array, words_to_find, puz_path)
+    solved_image = findword(array, words_to_find, puz_image[0])
+    solved_image.save("out.png")
 
 
 main()
